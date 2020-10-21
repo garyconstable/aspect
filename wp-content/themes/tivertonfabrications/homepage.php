b8e8fafc7cd608123fef562b2cd3923d3aa5bacd
Gary Constable <garyconstable80@gmail.com>
2020-10-21 23:25:24 +0100

    Updates.

--- wp-content/themes/tivertonfabrications/homepage.php
+++ wp-content/themes/tivertonfabrications/homepage.php
@@ -4,9 +4,6 @@
 
 $context                        = Timber::context();
 $timber_post                    = new Timber\Post();
-//$timber_post->custom['what_image']   = new \Timber\Image($timber_post->custom['what_image']);
 $context['post']                = $timber_post;
-
-//echo '<pre>'.print_r($context['post'] , true).'</pre>';die();
 
 Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'home.twig' ), $context );
